2c3938f3986b663628b0ff93a96b7d55769ead64
Tony <landf2000@gmail.com>
2021-01-16 15:42:52 -0500

    test

--- UbiGame_Blank/Source/Game/GameBoard.cpp
+++ UbiGame_Blank/Source/Game/GameBoard.cpp
@@ -7,7 +7,18 @@ using namespace Game;
 
 GameBoard::GameBoard()
 {
+	CreatePlayer(); 
+}
 
+void GameBoard::CreatePlayer()
+{
+	m_player = new GameEngine::Entity();
+	GameEngine::GameEngineMain::GetInstance()->AddEntity(m_player);
+
+	m_player->SetPos(sf::Vector2f(10.0f, 10.0f));
+	m_player->SetSize(sf::Vector2f(10.0f, 10.0f));
+
+	m_player->AddComponent<GameEngine::RenderComponent>();
 }
 
 
