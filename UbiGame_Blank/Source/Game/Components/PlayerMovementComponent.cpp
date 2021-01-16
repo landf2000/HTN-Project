99e460ad0ea52ff3381166fc009afc777beeb215
Tony <landf2000@gmail.com>
2021-01-16 17:36:20 -0500

    work [pls

--- UbiGame_Blank/Source/Game/Components/PlayerMovementComponent.cpp
+++ UbiGame_Blank/Source/Game/Components/PlayerMovementComponent.cpp
@@ -4,6 +4,8 @@
 
 #include "GameEngine/GameEngineMain.h" //<-- Add this include to retrieve the delta time between frames
 
+#include "GameEngine/EntitySystem/Components/PawnPhysicsComponent.h"
+
 using namespace Game;
 
 const float gravity = 5.0f;
@@ -13,8 +15,30 @@ void PlayerMovementComponent::Update()
        
 	Component::Update();
 
+    float playerVelocity = 100.f;
+
     //Grabs how much time has passed since last frame
     const float dt = GameEngine::GameEngineMain::GetTimeDelta();
+
+    sf::Vector2f wantedVelocity = sf::Vector2f(0.f, 0.f);
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+    {
+        wantedVelocity.x -= playerVelocity * dt;
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    {
+        wantedVelocity.x += playerVelocity * dt;
+    }
+
+    PawnPhysicsComponent* pawnPhys = GetEntity()->GetComponent<PawnPhysicsComponent>();
+    if (pawnPhys)
+    {
+        pawnPhys->SetVelocity(wantedVelocity);
+    }
+
+    /*
 
     //By default the displacement is 0,0
     sf::Vector2f displacement{ 0.0f,0.0f };
@@ -42,6 +66,8 @@ void PlayerMovementComponent::Update()
 
     //Update the entity position
     GetEntity()->SetPos(GetEntity()->GetPos() + displacement); 
+
+    */
 }
 
 void PlayerMovementComponent::OnAddToWorld() {}
--- UbiGame_Blank/Source/Game/GameBoard.cpp
+++ UbiGame_Blank/Source/Game/GameBoard.cpp
@@ -35,6 +35,8 @@ void GameBoard::CreatePlayer()
 	//Movement
 	m_player->AddComponent<Game::PlayerMovementComponent>();  // <-- Added the movement component to the player
 
+	
+
 	m_player->AddComponent<PawnPhysicsComponent>();
 
 	//m_player->AddComponent<GameEngine::CollidablePhysicsComponent>();
